50608eab488a418957ca954f3c0eb1f404204fb7
russphelan <russ.phelan@gmail.com>
2015-12-07 15:39:34 -0500

    quantum hand-integral correction matches plot next to eqn 75

--- differential part/equations.m
+++ differential part/equations.m
@@ -2,50 +2,61 @@
 %Date: 10-19-15
 
 function [a_prime] = equations(t,a,expandContract,simType,t0)
+
+%error conditions
+swError = 'The Expand/Contract switch parameter to the Friedmann equation was set improperly. Needs 1 or 0';
+zeroError = 'The quantum correction behavior is not defined for t=0';
+ 
+assert((expandContract == 0) || (expandContract == 1),swError);
+
 %implements equations for each simulation type
-
-%global parameters
-swError = 'The Expand/Contract switch parameter to the Friedmann equation was set improperly';
-p = 1/6/pi/t0^2; %energy density constant. In the case of stationary dust, this has only a mass contribution. 
-
 switch simType
+    
     case 0
-%friedman equation governs evolution of scale factor with time. set up in
-%case of pressureless dust. 
+        
+%CLASSICAL BEHAVIOR
 %requires a>0. Program will terminate if a_prime values become complex. 
 g = 1; %Newton's gravitational constant
+p = 1; %density of dust
 
 if expandContract==1
     a_prime = sqrt(8*pi*g*p/a);
 elseif expandContract==0
     a_prime = -sqrt(8*pi*g*p/a);
-else 
-    display(swError);
 end
 return;
+
     case 1
-%friedman equation with quantum correction. Correction is calculated as
-%integral by hand using the classical analytic ODE solution as the
-%differential part of the integro-differential equation.
+        
+%QUANTUM CORRECTION, HAND INTERGRALS
+%error checks
+assert(t0 ~= 0,zeroError);
 
 %parameters:
+p = 1/6/pi/t0^2; %energy density
 Ns = 1;
 Mr = 1;
-C1 = log(-1*Mr*t)/t;
-C2 = (log(-1*Mr*t)+1)/t^2;
-
+C1 = log(Mr*abs(t))/t;
+C2 = (log(Mr*abs(t))+1)/t^2;
 
 if expandContract==1
-    a_prime = sqrt( 1/a*(8*pi*p/3 + Ns/2430/pi*(19*C1/t0^2/t + 26*C2/t0^2)));
+    assert(false, 'expansion behavior is not implemented for the quantum correction');
 elseif expandContract==0
     a_prime = -1*sqrt( 1/a*(8*pi*p/3 + Ns/2430/pi*(19*C1/t0^2/t + 26*C2/t0^2)));
-else 
-    display(swError);
+    if ~isreal(a_prime)
+        disp(sprintf('the value for :a: is: %d /n',a))
+        disp(sprintf('the value for :gamma: is: %d /n',(8*pi*p/3 + Ns/2430/pi*(19*C1/t0^2/t + 26*C2/t0^2))))
+        disp(sprintf('the value for :alpha: is: %d /n',8*pi*p/3))
+        disp(sprintf('the value for :beta: is: %d /n',Ns/2430/pi*(19*C1/t0^2/t + 26*C2/t0^2)))
+        disp(sprintf('the value for :firstlog: is: %d /n',19*C1/t0^2/t))
+        disp(sprintf('the value for :secondlog: is: %d /n',26*C2/t0^2))
+        assert(isreal(a_prime),'The scale factor deriv has become imaginary');
+    end
 end
 return;
+
     case 2
-        display('The full quantum simulation has not yet been implemented');
-        return;
+        %NOT YET IMPLEMENTED
 end
 end
 
--- differential part/runge_kutta.m
+++ differential part/runge_kutta.m
@@ -5,12 +5,13 @@ clear all;
 
 %user dialog, initial conditions
 simType = input('Choose simulation type: \n(0)Classical Friedmann \n(1)Quantum Approximation \n(2)Full Quantum Behavior');
+assert(simType ~= 2,'Full quantum correction not yet implemented');
 t0 = input('Enter initial time: (seconds)');
 endInt = input('Enter end time: (seconds)');
 a0 = input('Enter intial scale factor value: (dimensionless)');
 sw = input('Type 1 for expanding universe, 0 for contracting universe.');
 
-step = .001; %step time in seconds
+step = .001; %step size in seconds
 totalSteps = (endInt-t0)/step;
 totalSteps = round(totalSteps);
 
@@ -29,7 +30,11 @@ t_array = t0;
 
 %performing iterations
 for i=1:totalSteps
-   
+    
+    %error checks: 
+    assert(old_a>=0, 'behavior is not defined for a negative scale factor');
+    assert(~isnan(next_a),'The scale factor is not a number');
+    
     k1 = equations(oldT,old_a,sw,simType,t0);
     k2 = equations(oldT + step/2,old_a + (step/2)*k1,sw,simType,t0);
     k3 = equations(oldT + step/2,old_a + (step/2)*k2,sw,simType,t0);
@@ -37,9 +42,7 @@ for i=1:totalSteps
    
     nextT = oldT + step;
     next_a = old_a + (step/6)*(k1 + 2*k2 + 2*k3 + k4);
-    % if imag(next_a)~=0
-    %    break;
-   %end
+   
     a_array = [a_array next_a];
     t_array = [t_array nextT];
     
@@ -49,30 +52,29 @@ end
 
 %derivative calculations
 aPrime_array = derivOutputArray(a_array,t_array);
-
 aDoublePrime_array = derivOutputArray(aPrime_array,t_array);
 
 %plotting
+
+lw = 1; %sets linewidth for all plots
 subplot(2,2,1);
 
-plot(t_array,a_array,'LineWidth',2);
+plot(t_array,a_array,'LineWidth',lw);
 
 %labels
 xlabel('Time (s)','FontSize',14,'interpreter','latex');
 ylabel('$a(t)$','FontSize',14,'interpreter','latex');
 title('Scale Factor','FontSize',18,'FontWeight','bold','interpreter','latex');
 
-
-
 subplot(2,2,2);
-plot(t_array,aPrime_array,'LineWidth',2);
+plot(t_array,aPrime_array,'LineWidth',lw);
 
 xlabel('Time (s)','FontSize',14,'interpreter','latex');
 ylabel('$\dot{a}(t)$','FontSize',14,'interpreter','latex');
 title('Derivative','FontSize',18,'FontWeight','bold','interpreter','latex');
 
 subplot(2,2,3);
-plot(t_array,aDoublePrime_array,'LineWidth',2);
+plot(t_array,aDoublePrime_array,'LineWidth',lw);
 xlabel('Time (s)','FontSize',14,'interpreter','latex');
 ylabel('$\ddot{a}(t)$','FontSize',14,'interpreter','latex');
 title('Second Derivative','FontSize',18,'FontWeight','bold','interpreter','latex');
