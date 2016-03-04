2e0747a672aabbf129faafe4a67c1c397d687038
Russell Phelan <russ.phelan@gmail.com>
2016-03-04 12:49:00 -0500

    cleanup of what we have so far. this does derivatives, plots them, and makes complete sense. good starting point for rectangle rule solve

--- integro-differential solver/solver.m
+++ integro-differential solver/solver.m
@@ -4,11 +4,14 @@
 
 clear all; 
 
+%global settings
+step = .001; %in seconds
+
+
 %THIS SHOULD BE CLEANED UP WHEN YOU'RE DONE WITH EVERYTHING ELSE
 
 %user dialog, initial conditions
 simType = input('Choose simulation type: \n(0)Classical Friedmann \n(1)Quantum Approximation \n(2)Full Quantum Behavior');
-
 assert(simType ~= 2,'Full quantum correction not yet implemented');
 
 t0 = input('Enter initial time: (seconds)');
@@ -17,14 +20,13 @@ a0 = input('Enter intial scale factor value: (dimensionless)');
 sw = input('Type 1 for expanding universe, 0 for contracting universe.');
 
 %setting up steps
-step = .001; %in seconds
 totalSteps = (endInt-t0)/step;
-totalSteps = round(totalSteps) + 1;
+totalSteps = round(totalSteps) + 1; %+1 ensures evaluation at end time given...I know it looks strange.
 
 %setting up scale factor a(t) function object, and derivatives
-[scale_factor,scale_1deriv,scale_2deriv] = deal(NaN(2,totalSteps));
+[scale_factor,scale_1deriv,scale_2deriv] = deal(NaN(2,totalSteps)); %fill them with NaNs
 for i=1:totalSteps
-    [scale_factor(2,i),scale_1deriv(2,i),scale_2deriv(2,i)] = deal((i-1)*step);
+    [scale_factor(2,i),scale_1deriv(2,i),scale_2deriv(2,i)] = deal((i-1)*step); %fill second row with t values spaced with 'step'
 end
 scale_factor(1,1) = a0; %initial conditions
 scale_factor(2,1) = t0; 
@@ -36,10 +38,10 @@ for i=1:totalSteps-1
     scale_factor = runge_step(scale_factor,step,sw,simType); %runge-kutta algorithm
     
     %calculate next set of derivative steps, update arrays
-    if(i>1)
+    if(i>1)%filled with NaNs until i=2
         scale_1deriv(1,i) = slope_btwn(scale_factor,i,i-1);
     end
-    if(i>2)
+    if(i>3) %filled with NaNs till i=4
         scale_2deriv(1,i) = slope_btwn(scale_1deriv,i-1,i-2);
     end
     
